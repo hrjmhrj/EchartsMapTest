10bd04e6ab4aa10ccc3e322e8d65163f3d0c89ff
hrj <412222753@qq.com>
2020-09-22 10:44:57 +0800

    geojson

--- src/components/demo/App4.vue
+++ src/components/demo/App4.vue
@@ -4,12 +4,23 @@
         <div :style="{width:'100vw'}" class="echart1" :id="dtdivid+'1'"></div>
         <div :style="{width:'100vw'}" class="echart2" :id="dtdivid+'2'"></div>
         <div :style="{width:'100vw'}" class="echart3" :id="dtdivid+'3'"></div>
+        <div v-show="this.show" @click="closediv" :style="{width:'100vw'}" class="echart4" :id="dtdivid+'4'"></div>
     </div>
 </template>
 
 <script>
     import xindoudt from '../../../static/json/xindouditumap.json'//引进底图
     import xdwapiant from '../../../static/json/xindoumap.json'//引进线条图形
+    import shibantan from '../../../static/json/shibantan.json'//引进石板滩街道
+    import banzuyuan from '../../../static/json/banzuyuan.json'//引进斑竹园街道
+    import dafeng from '../../../static/json/dafeng.json'//引进大丰街道
+    import guihu from '../../../static/json/guihu.json'//引进桂湖街道
+    import juntun from '../../../static/json/juntun.json'//引进军屯镇
+    import qingliu from '../../../static/json/qingliu.json'//引进清流镇
+    import sanhe from '../../../static/json/sanhe.json'//引进三河街道
+    import xindou from '../../../static/json/xindou.json'//引进新都街道
+    import xinfan from '../../../static/json/xinfan.json'//引进新繁街道
+
     const testDemo = []
     testDemo.push({name: '斑竹园街道', value: '99'})
     testDemo.push({name: '大丰街道', value: '11'})
@@ -36,19 +47,6 @@
         testData.push({
             name: testDemo[i].name,
             value: testDemo[i].value,
-            label: {
-                normal: {//常态
-                    show: true,
-                    align: 'center',
-                    verticalAlign: 'middle',
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    color: 'white',
-                    formatter: '{b}\n{c}万',
-                },
-                emphasis:{//选中状态
-                },
-            }
         })
     }
     export default {
@@ -56,12 +54,26 @@
         components: {},
         data() {
             return {
+                show: false,
                 dtdivid:'map',
                 chart: null,
                 chart2: null,
                 chart3: null,
                 chart4: null,
-                testData: testData
+                testData: testData,
+                json:{
+                    xindoudt:xindoudt,
+                    xdwapiant:xdwapiant,
+                    shibantan:shibantan,
+                    banzuyuan:banzuyuan,
+                    dafeng:dafeng,
+                    guihu:guihu,
+                    juntun:juntun,
+                    qingliu:qingliu,
+                    sanhe:sanhe,
+                    xindou:xindou,
+                    xinfan:xinfan,
+                }
             }
         },
         mounted() {
@@ -72,32 +84,20 @@
         },
         methods: {
             inintMap() {
-                this.chart = this.$echarts.init(document.getElementById(this.dtdivid))
-                this.$echarts.registerMap('inintMap', xdwapiant)
-                this.chart.on('click',function (params) {//点击事件
-                    console.log(params);
+                let _this = this
+                _this.chart = _this.$echarts.init(document.getElementById(_this.dtdivid))
+                _this.$echarts.registerMap('inintMap', xdwapiant)
+                _this.chart.on('click',function (params) {//点击事件
+                    //console.log(params);
                     if( testName.hasOwnProperty(params.name)){
-                        console.log("0000000000000000000000000");
-                        $.getJSON('static/json/'+ testName[params.name] +'.json', function(data){
-                            this.$echarts.registerMap(params.name, data);
-                            console.log(data);
-                            let d = [];
-                            for( let i=0;i<data.length;i++ ){
-                                d.push({
-                                    name:data[i].name
-                                })
-                            }
-                            console.log(d);
-                            this.$echarts.renderMap(params.name,d);
-                        });
+                        //console.log("0000000000000000000000000");
+                        //console.log(testName[params.name]);
+                        _this.tcmap(testName[params.name]);
                     }else {
-                        console.log("22222222222222222222222222222");
-                        console.log(params.name);
-                        console.log("1111");
-                        console.log(testName);
+                        console.log("点击事件出错，不包含点击的地图");
                     }
                 })
-                this.chart.setOption({
+                _this.chart.setOption({
                     visualMap: {
                         show: false,
                         type: 'piecewise',
@@ -138,7 +138,10 @@
                                     fontWeight: 'bold',
                                     color: 'white'
                                 }
-                            }
+                            },
+                            emphasis:{//选中状态
+                                show: false,
+                            },
                         },
                         aspectScale:0.85,//长宽比
                         zoom:1,//放大比例
@@ -164,20 +167,20 @@
                         },
                         label: {//地图文本
                             normal: {//常态
+                                show: true,
                                 align: 'center',
                                 verticalAlign: 'middle',
                                 fontSize: 16,
                                 fontWeight: 'bold',
                                 color: 'white',
+                                formatter: '{b}\n{c}万',
                             },
                             emphasis:{//选中状态
-                            }
+                            },
                         },
                     },
                     //点
                         {
-                            // scatter  heatmap ,
-                            //name: '基金小镇',
                             type: 'effectScatter',
                             legendHoverLink: true,
                             coordinateSystem: 'geo',
@@ -225,6 +228,14 @@
                                 },
                                 {name: '三号公司', value: [104.12567138671875, 30.85123693902637],
                                     tooltip:{formatter:'{b}'},
+                                },{name: '新繁初号机', value: [104.00619506835936,30.873940237887624],
+                                    tooltip:{formatter:'{b}'},
+                                },
+                                {name: '新繁二号点', value: [104.00919506835936,30.875940237887624],
+                                    tooltip:{formatter:'{b}'},
+                                },
+                                {name: '新繁三号', value: [104.00119506835936,30.871940237887624],
+                                    tooltip:{formatter:'{b}'},
                                 },
                             ],
                         }
@@ -233,12 +244,12 @@
             },
             inintCharts1() {
                 this.chart2 = this.$echarts.init(document.getElementById(this.dtdivid+'1'))
-                this.$echarts.registerMap('mymap1', xindoudt)
+                this.$echarts.registerMap('showmap1', xindoudt)
                 this.chart2.setOption({
                     series: [
                         {
                             type: 'map',
-                            mapType: 'mymap1',
+                            mapType: 'showmap1',
                             center: [104.12567138671875, 30.84623693902637],
                             zoom: 1,
                             aspectScale: 0.85, // 长宽比
@@ -253,12 +264,12 @@
             },
             inintCharts2() {
                 this.chart3 = this.$echarts.init(document.getElementById(this.dtdivid+'2'))
-                this.$echarts.registerMap('mymap2', xindoudt)
+                this.$echarts.registerMap('showmap2', xindoudt)
                 this.chart3.setOption({
                     series: [
                         {
                             type: 'map',
-                            mapType: 'mymap2',
+                            mapType: 'showmap2',
                             center: [104.12567138671875, 30.84623693902637],
                             zoom: 1,
                             aspectScale: 0.85, // 长宽比
@@ -273,12 +284,12 @@
             },
             inintCharts3() {
                 this.chart4 = this.$echarts.init(document.getElementById(this.dtdivid+'3'))
-                this.$echarts.registerMap('mymap3', xindoudt)
+                this.$echarts.registerMap('showmap3', xindoudt)
                 this.chart4.setOption({
                     series: [
                         {
                             type: 'map',
-                            mapType: 'mymap3',
+                            mapType: 'showmap3',
                             center: [104.12567138671875, 30.84623693902637],
                             zoom: 1,
                             aspectScale: 0.85, // 长宽比
@@ -291,14 +302,155 @@
                     ]
                 })
             },
+            tcmap(name) {
+                this.show = true;
+                this.chart4 = this.$echarts.init(document.getElementById(this.dtdivid+'4'))
+                this.$echarts.registerMap('showmap4', this.json[name])
+                this.chart4.setOption({
+                    visualMap: {
+                        show: false,
+                        type: 'piecewise',
+                        min: 0,//最小值
+                        max: 100,//最大值  用下面的颜色数组实现热力体现
+                        calculable: false, //是否显示拖拽用的手柄
+                        realtime: false,//拖拽时，是否实时更新
+                        inverse:false, //是否反转 visualMap 组件
+                        splitNumber: 4,//对于连续型数据，自动平均切分成几段。默认为5段
+                        right: 20,//组件离容器右侧的距离,‘20%’
+                        bottom: 50, //组件离容器下侧的距离,‘20%’
+                        inRange: {//定义 在选中范围中 的视觉元素
+                            // color: ['#3674e8', '#38de91', '#fbf75b', '#ff8f14']
+                            color: ['rgba(0,153,255, 0.5)', 'rgba(37,185,155, 0.5)', 'rgba(238,194,17, 0.5)', 'rgba(255,0,0, 0.5)']
+                        },
+                    },
+                    tooltip:{
+                        formatter:'{b}<br/>{c}万',//格式化鼠标聚焦时显示的内容
+                        position: ['35%', '80%']//位置比例 距离左边 距离上部
+                    },
+                    geo: {
+                        map: 'showmap4',  //必须以注册的地图名称一致
+                        //center: [104.00619506835936,30.873940237887624],//中心点 就是页面显示以这个点为中心
+                        itemStyle: {
+                            normal: {
+                                borderColor: 'rgba(255,255,255, 0.5)',//瓦片的边 半透明白色
+                                borderWidth: 4//边粗
+                            }
+                        },
+                        label: {
+                            normal: {
+                                textStyle: {//文本
+                                    fontSize: 16,
+                                    fontWeight: 'bold',
+                                    color: 'white'
+                                }
+                            },
+                            emphasis:{//选中状态
+                                show: false,
+                            },
+                        },
+                        aspectScale:0.85,//长宽比
+                        zoom:1,//放大比例
+                    },
+                    //图
+                    series: [
+                        {
+                            type: 'map',
+                            mapType: 'showmap4',
+                            data: testData,
+                            //center: [104.00619506835936,30.873940237887624],//中心点 就是页面显示以这个点为中心
+                            zoom: 1,//放大比例
+                            aspectScale: 0.85, // 长宽比
+                            silent: true, //响应和触发鼠标事件
+                            itemStyle: {//图形样式。
+                                normal: {//常态
+                                    borderColor: 'rgba(187,187,187, 0.5)',
+                                    borderWidth: 4
+                                },
+                                emphasis:{//选中状态
+                                },
+                            },
+                            label: {//地图文本
+                                normal: {//常态
+                                    show:true,
+                                    textStyle: {
+                                        ontSize: 16,
+                                        fontWeight: 'bold',
+                                        color: 'white'
+                                     },
+                                    align: 'center',
+                                    verticalAlign: 'middle',
+                                    fontSize: 16,
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                },
+                                emphasis:{//选中状态
+                                    color: 'white'
+                                },
+                            },
+                        },
+                        //点
+                        {
+                            type: 'effectScatter',
+                            legendHoverLink: true,
+                            coordinateSystem: 'geo',
+                            //center: [104.00619506835936,30.873940237887624],//中心点 就是页面显示以这个点为中心
+                            zoom: 1,
+                            aspectScale: 0.85, // 长宽比
+                            symbol: 'diamond',
+                            label: {
+                                normal: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    show: false
+                                }
+                            },
+                            itemStyle: {
+                                normal: {
+                                    borderColor: 'rgba(255,255,255, 0.5)',
+                                    borderWidth: 4
+                                },
+                                emphasis: {
+                                    borderColor: '#fff',
+                                    borderWidth: 1
+                                }
+                            },
+                            // 文本位置修正
+                            textFixed: {
+                                Alaska: [20, -20]
+                            },
+                            rippleEffect: {
+                                scale: 8,
+                                brushType: 'stroke',
+                            },
+                            legendHoverLink: true,//点波纹动态效果
+                            symbolSize: 12,
+                            /*function (val) {//根据数值大小控制点的大小
+                               return val[2] / 1;
+                           },*/
+                            data: [//jjxzscData
+                                {name: '新繁初号机', value: [104.00619506835936,30.873940237887624],
+                                    tooltip:{formatter:'{b}'},
+                                },
+                                {name: '新繁二号点', value: [104.00919506835936,30.875940237887624],
+                                    tooltip:{formatter:'{b}'},
+                                },
+                                {name: '新繁三号', value: [104.00119506835936,30.871940237887624],
+                                    tooltip:{formatter:'{b}'},
+                                },
+                            ],
+                        }
+                    ]
+                })
+            },
+            closediv(){
+                this.show = false
+            },
         },
     }
 </script>
 
 <style scoped>
-    .anchorBL {
-        display: none;
-    }
     /*中间div*/
     .mapDiv {
         height: 87.3vh;
@@ -338,6 +490,14 @@
         height: 90vh;
         padding-left: 1%;
         padding-top: 4%;
+    }
+
+    /*4弹出的瓦片图*/
+    .echart4 {
+        position: absolute;
+        z-index: 160;
+        height: 90vh;
+        background: #fff!important;
     }
 
 
