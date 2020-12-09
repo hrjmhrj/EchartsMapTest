64cd910b275b7eb7110ee3370abfbba4f75aaf7f
hrj <412222753@qq.com>
2020-12-09 15:10:50 +0800

    修改版本

--- src/router/index.js
+++ src/router/index.js
@@ -3,30 +3,36 @@ import Router from 'vue-router'
 import browser from '@/components/browser.vue'
 import map from '@/components/map.vue'
 import demo from '@/components/demo/hjzx/homehjzx.vue' //分辨率 3840 2160
-//import demo from '@/components/demo/hjzx/App4.vue'
+import xdmap from '@/components/demo/App4.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-	 {
-	    path: '/',
-	    name: 'demo',
-	    component: demo
-	  },
-    {
-      path: '/browser',
-      name: 'browser',
-      component: browser
-    },
-    {
-        path: '/map',
-        name: 'map',
-        component: map
-      },
+    routes: [
+        {
+            path: '/',
+            name: 'demo',
+            component: demo
+        },
+        {
+            path: '/browser',
+            name: 'browser',
+            component: browser
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: map
+        },
         {
             path: '/goalMap',
             name: 'demo',
             component: demo
-          }
-  ]
+        },
+        {
+            path: '/testmap',
+            name: 'xdmap',
+            component: xdmap
+        }
+    ]
 })
